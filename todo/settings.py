309d79fa1611b2ecc7c00241c297ae97bbb5180d
Peter Kosinski <peter.kosinski.irl@gmail.com>
2017-11-10 12:55:07 +0000

    6th commit

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-todo-peterkosinski.c9users.io',]
+ALLOWED_HOSTS = ['django-todo-peterkosinski.c9users.io','com-peterkosinski-simpletodo.herokuapp.com']
 
 
 # Application definition
